5778e0f3098c2b2b42cedf81f0de48a871fbf121
Simon Klingler <simon-klingler@web.de>
2022-12-11 12:31:37 +0100

    Updated solution

--- aoc8/solution.hs
+++ aoc8/solution.hs
@@ -1,19 +1,25 @@
 import Data.List (transpose, tails, mapAccumL)
 
--- Number trees visible from the outside
-numTreesVisible :: [[Integer]] -> Int
-numTreesVisible trees = length . filter id . map (any id) . transpose $ allViews
+-- Number trees visible from the outside of the rectangular forest.
+numTreesVisible :: [[Integer]] -> Integer
+numTreesVisible trees = fromIntegral . length . filter (any id) . transpose $ allViews
   where allViews = (concat .) <$> [view, down, right, up] <*> [trees]
-        down     = transpose . view . transpose
-        right    = map reverse . view . map reverse
-        up       = transpose . right . transpose
+        down     = transpose . view . transpose      -- Check visibility of all trees from below.
+        right    = map reverse . view . map reverse  -- Check visibility of all trees from the right.
+        up       = transpose . right . transpose     -- Check visibility of all trees from above.
 
--- Number of trees visible from the left
+-- Number of trees visible from the left.
 view :: [[Integer]] -> [[Bool]]
 view = map $ snd . mapAccumL (\x y -> (max x y, x < y)) (-1)
 
--- Read input from file, output solution
+-- Read the input from a specified file.
+readInput :: String -> IO [[Integer]]
+readInput path = map (map $ read . (: [])) . lines <$> readFile path :: IO [[Integer]] 
+
+-- Read input and solve.
+solve :: String -> IO ()
+solve path = readInput path >>= print . numTreesVisible
+
+-- Read input from file, output solution.
 main :: IO ()
-main = do
-    input <- map (map $ read . (: [])) . lines <$> readFile "input.txt" :: IO [[Integer]]
-    print $ numTreesVisible input
+main = solve "input.txt"
